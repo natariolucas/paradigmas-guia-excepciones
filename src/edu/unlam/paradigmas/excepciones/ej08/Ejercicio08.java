aa1caea1faf74bec56b41b87bb6e6b7d3169ee34
Lucas Natario <lucasenatario@gmail.com>
2024-05-13 23:42:58 -0300

    Assertions

--- src/edu/unlam/paradigmas/excepciones/ej08/Ejercicio08.java
+++ src/edu/unlam/paradigmas/excepciones/ej08/Ejercicio08.java
@@ -5,6 +5,7 @@ public class Ejercicio08 {
 	public Ejercicio08() { }
 	
 	public static double Cociente(int a, int b) {
+		// Enabled in Run -> rung configurations -> {{application}} -> arguments -> "-ea"
 		assert b != 0 : "Error! no se puede dividir por cero";
 		
 		return a/b;
